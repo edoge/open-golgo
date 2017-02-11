63eef89556871098f1c84b47dc8b622c666b3c25
tetuyosi <tetuyosi39@gmail.com>
2017-02-11 18:08:24 +0900

    add rank

--- src/app/Console/Commands/IrcClient.php
+++ src/app/Console/Commands/IrcClient.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Hoa;
+use App\Models\Players;
 
 class IrcClient extends Command
 {
@@ -28,21 +29,52 @@ class IrcClient extends Command
      */
     public function handle()
     {
-        $uri    = 'irc://irc.ircnet.ne.jp:6662';
+        $uri    = 'irc://irc.livedoor.ne.jp:6662';
         $client = new Hoa\Irc\Client(new Hoa\Socket\Client($uri));
 
         $client->on('open', function (Hoa\Event\Bucket $bucket) {
-            $bucket->getSource()->join('g_bot', '#open-golgo');
+            $bucket->getSource()->join('open_g', '#open-golgo');
             return;
         });
-        $client->on('mention', function (Hoa\Event\Bucket $bucket) {
+        $client->on('message', function (Hoa\Event\Bucket $bucket) {
             $data    = $bucket->getData();
-            $message = $data['message']; // do something with that.
-
+            $message = $data['message'];
+            $words = explode(' ', $message);
+            switch(trim(head($words))) {
+                case "tg":
+                    $bucket->getSource()->say("not implemented.");
+                    break;
+                case "rank":
+                    if (count($words) !== 2){
+                        $bucket->getSource()->say("usage: rank <target>.");
+                        return;
+                    }
+                    $player = Players::where('name', trim($words[1]))->first();
+                    if (is_null($player)) {
+                        $bucket->getSource()->say(trim($words[1]) . " is not found.");
+                    } else {
+                        $bucket->getSource()->say($player->name . "'s rank is ". $player->rank);
+                    }
+                    break;
+                default:
+                    return;
+            }
+            return;
+        });
+        $client->on('mention', function(Hoa\Event\Bucket $bucket) {
+            $data    = $bucket->getData();
+            $message = $data['message'];
+            if (strpos($message, 'crawl') > 0) {
+                \Illuminate\Support\Facades\Artisan::call('boot:crawl', []);
+                $bucket->getSource()->say("done.");
+                return;
+            } else if (strpos($message, 'join') > 0) {
+                $bucket->getSource()->join('open_g', '#roror');
+                return;
+            }
             $bucket->getSource()->say(
                 $data['from']['nick'] . ': What?'
             );
-            return;
         });
         $client->run();
     }
