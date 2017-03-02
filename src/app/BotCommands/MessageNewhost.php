48be5fbff9777cb9bcb5944d0fb1a7d5b6cc8cb3
tetuyosi <tetuyosi39@gmail.com>
2017-03-02 23:14:26 +0900

    new host by ip address

--- src/app/BotCommands/MessageNewhost.php
+++ src/app/BotCommands/MessageNewhost.php
@@ -6,7 +6,7 @@ class MessageNewhost
 {
     public function handle($message, $from)
     {
-        $ret = $from['nick'] . ' ■新規干す ' . $from['host'] . ' @' . mb_substr($message, -1);
+        $ret = $from['nick'] . ' ■新規干す ' . gethostbyname($from['host']) . ' @' . mb_substr($message, -1);
         return mb_convert_encoding($ret, config('app.irc_encoding'));
     }
 }
